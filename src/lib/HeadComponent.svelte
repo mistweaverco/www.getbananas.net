d9bb9e20b37b06c4a8a932351805e87d839a1cfe
Marco Kellershoff <marco@kellershoff.net>
2024-11-29 12:15:17 +0100

    feat(ux): add open-graph image

--- src/lib/HeadComponent.svelte
+++ src/lib/HeadComponent.svelte
@@ -5,6 +5,7 @@
 <svelte:head>
 	<title>{data.title}</title>
 	<meta name="description" content={data.description} />
+	<meta property="og:image" content="/open-graph.png" />
 	<style>
 		@import '$lib/styles.css';
 	</style>
